4778dc582729d02fc2a9c3415d648b2209224d8e
Gaurav27r <82757143+Gaurav27r@users.noreply.github.com>
2021-05-14 22:13:10 -1000

    Update background.js

--- AdBlocker-main/background.js
+++ AdBlocker-main/background.js
@@ -3,6 +3,7 @@ chrome.webRequest.onBeforeRequest.addListener(
 	{ urls: ["*://*.code.whitehatjr.com/*","*://*.mgmotor.co.in/*","*://*.zedo.com/*", "*://*.google.com/pagead/*", "*://*.google.com/aclk*","*://*.google.co.in/pagead/*", "*://*.youtube.com/pagead/*","*://*.youtube.com/api/stats/ads*", "*://*.codr.toppr.com/*", "*://*.wizklub.com/*"]},
 	["blocking"]
 )
+//changes
 //append the list in line 3 to block more sites
 //eg:-
 // "*://*.xyz.com/*"
